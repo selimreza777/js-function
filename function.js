b2eb37d04bd2b380069f9120dcd1cfa25a2fb7aa
selimreza777 <selimrezacse@gmail.com>
2025-08-10 21:49:47 +0600

    parameter

---
+++ function.js
@@ -0,0 +1,44 @@
+// function decleration 
+
+function fanOffKor() {
+  console.log('fan off');
+  console.log('fan on');
+  console.log('fan speed 5');
+}
+// overall strucuter
+//function name(){
+// function er body
+//}
+
+function brashTeeth() {
+  console.log('pick up the brash');
+  console.log('add pest');
+  console.log('2 min goso and goso and fena felo');
+}
+// call function
+// brashTeeth();
+// console.log('----------');
+// console.log('--------------');
+// console.log('tom and jerey');
+// brashTeeth();
+
+// Macher recepi 
+
+// function koyMachRecepi(mach, shobji, moshla) {
+//   // rannar niyomaboli 
+//   console.log('korai gorom korkun');
+//   console.log('2 tab chamuch tel korai te dhalun and gorm korun');
+//   console.log('mach gula kori te dhalun and 5min fry korun');
+//   console.log('korai te shobji din and narun');
+//   return "shudhadu koy macher jhol!";
+// }
+// const result =koyMachRecepi("koy","law","holud");
+// console.log(result);
+
+function vattKhaw(){
+  console.log("hand wash koro");
+  console.log("plate naw");
+  console.log("vat shobji naw");
+  console.log("gopat gopat kore khaw");
+}
+// vattKhaw();
---
+++ parameter.js
@@ -0,0 +1,20 @@
+function square(number) {
+  // console.log(number);
+  console.log("given the parameter number is", number);
+  const borgo = number * number;
+  console.log("square the given number", borgo)
+  return borgo;
+}
+// const result = square(11);
+// console.log("square is:", result);
+// square(5);
+// square(8);
+// square(302);
+
+function add(num1,num2,num3){
+  console.log("parmeter number is:",num1,num2,num3);
+  const addedNumber=num1+num2+num3;
+  return addedNumber;
+}
+const result=add(2,3,5);
+console.log(result);
